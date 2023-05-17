01b939ec332f65c3e17d41a4b57a2334269c25f9
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-05-17 19:43:55 +0200

    main

--- lanzador.py
+++ lanzador.py
@@ -2,13 +2,13 @@ from clases.barbero import Barbero
 from clases.barberia import Barberia
 from clases.clientes import Cliente
 
-
-barberia = Barberia(3)
-barbero = Barbero(barberia)
-cliente1 = Cliente(barberia)
-cliente2 = Cliente(barberia)
-cliente3 = Cliente(barberia)
-barbero.start()
-cliente1.start()
-cliente2.start()
-cliente3.start()
+def main():
+    barberia = Barberia(3)
+    barbero = Barbero(barberia)
+    cliente1 = Cliente(barberia)
+    cliente2 = Cliente(barberia)
+    cliente3 = Cliente(barberia)
+    barbero.start()
+    cliente1.start()
+    cliente2.start()
+    cliente3.start()
